ec12e40e479ec3aa0451daf7e000658076603a5d
MioHagiwara30 <arawigah52@gmail.com>
2023-01-14 23:47:00 +0900

    バリデーションの追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,12 +6,21 @@
 
   <div class="tweet-wrapper">
   <div class="tweet-wrapper-inside">
-   <img class="icon-tweet" src="images/dawn.png">
+   <img class="icon-tweet" src="storage/images/{{$auth->images}}">
 
    <div class="form-group">
    <form action="/post/create" method="post">
   @csrf
 <textarea name="newPost" placeholder="何をつぶやこうか…？" style="width:350px; height:70px;" required></textarea>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 </div>
 <input type="image" class="icon-sent" src=images/post.png>
 </form>
@@ -28,7 +37,7 @@
 @foreach($posts as $post)
 
  <tr class="detailgroup">
- <th><img class="icon-tweet" src="images/dawn.png"></th>
+ <th><img class="icon-tweet" src="storage/images/{{$post->images}}"></th>
        <th class="tweet_username">{{ $post -> username }}</th>
        <th class="tweet_time">{{ $post -> created_at }}</th>
  </tr>
@@ -37,50 +46,43 @@
 
        <!-- 編集ボタン -->
 @if($post->user_id==$auth->id)
-       <th> <a href="" class="modalopen" data-target="modal_01">
-       {!! Form::input('hidden','id',$post->user_id, ['required', 'class' => 'form-control']) !!}
-        <img class="icon-edit" src="images/edit.png"></a>
-        {!! Form::close() !!}
+       <th> <div class="modalopen" data-target="{{$post->id}}">
+        <img class="icon-edit" src="images/edit.png"></div>
        </th>
-       </div>
              <!-- モーダル 中身-->
-  <div class="modal_hidden js-modal" id="modal_01">
+  <div class="modal_hidden js-modal" id="{{$post->id}}">
+    <!-- 黒背景 -->
    <div class="modal-inner">
-    <div class="inner-content">
-    <div class='container'>
-        <h2 class='page-header'>投稿内容の編集</h2>
-        {!! Form::open(['url' => 'post/update']) !!}
-        <div class="form-group">
-        {!! Form::input('hidden','id',$post->user_id, ['required', 'class' => 'form-control']) !!}
-            {!! Form::input('text','upPost',$post->posts, ['required', 'class' => 'form-control']) !!}
-        </div>
-        <button type="submit" class="btn btn-success pull-right">編集</button>
-        {!! Form::close() !!}
-    </div>
-      <a class="send-button modalClose" href="top">Close</a>
+    <!-- 中の白背景 -->
+    <div class='tweet_update_container'>
+      <div class=""update-inner>
+       <form action="/post/update" method="post">
+        @csrf
+        <input type="hidden" name="id" value="{{ $post->id }}">
+        <input type="textarea" name="upPost" value="{{ $post->posts }}" style="width:350px; height:70px;" required  class="update-inner">
+
+            @if ($errors->any())
+              <div class="alert alert-danger">
+                <ul>
+                 @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                 @endforeach
+                </ul>
+              </div>
+            @endif
+      </div>
+        <button type="submit">
+          <img class="icon-edit2" src="images/edit.png">
+        </button>
+
+      </form>
+      <div class="modalClosetext">
+       <a href="top">× Close</a>
+      </div>
     </div>
   </div>
-
-<!-- モーダル中身ここまで -->
-
-<script>
-jQuery(function ($) {
-  $('.modalopen').on('click', function () {
-      console.log(modal);
-      $(modal).fadeIn();
-      return false;
-    });
-  });
-  $('.modalClose').on('click', function () {
-    $('.js-modal').fadeOut();
-    return false;
-  });
-});
-
-</script>
-
 <!-- 削除ボタン -->
-       <th> <a class="delete-btn" href="post/{{ $post->id }}/delete" onclick="return confirm('この投稿を削除しますか？')"><img class="icon-delete" src="images/trash.png"></a> </th>
+       <th> <a class="delete-btn" href="post/{{ $post->id }}/delete" onclick="return confirm('この投稿を削除しますか？')"><img class="icon-delete" src="images/trash.png" onmouseover="this.src='images/trash_h.png'" onmouseout="this.src='images/trash.png'" ></a> </th>
        @endif
  </tr>
 
